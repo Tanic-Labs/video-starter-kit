0b93b35498a8114615ddb820d39f04466c42d71c
Ailam Cabello <ailamcabello@192.168.2.155>
2025-01-27 12:21:07 -0600

    create-supabse-client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 import { App } from "@/components/main";
 import { PROJECT_PLACEHOLDER } from "@/data/schema";
 import { cookies } from "next/headers";
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase: SupabaseClient<any> = createClient(supabaseUrl, supabaseKey);
-//const supabase = {fake: true};
 
 export default async function IndexPage() {
   const cookieStore = await cookies();
   const lastProjectId = cookieStore.get("__aivs_lastProjectId");
 
-  //console.log("supabase: ", supabase);
-
   return (
     <>
       <App
         projectId={lastProjectId?.value ?? PROJECT_PLACEHOLDER.id}
-        supabase={supabase}
+        supabaseUrl={supabaseUrl}
+        supabaseKey={supabaseKey}
       />
     </>
   );
