cffe7552ebb3abbbfb953b1d81141a6ba9d7d522
Ailam Cabello <ailamcabello@192.168.2.146>
2025-02-14 12:32:48 -0600

    close-media-gallery-button

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -61,9 +61,9 @@ const SheetPanel = React.forwardRef<
     {...props}
   >
     {children}
-    <SheetPrimitive.Close 
+    <SheetPrimitive.Close
       className="ring-offset-background focus:ring-ring absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
-      onClick={onClose}  
+      onClick={onClose}
     >
       <X className="h-4 w-4" />
       <span className="sr-only">Close</span>
@@ -75,8 +75,8 @@ SheetPanel.displayName = "SheetPanel";
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {
-      onClose?: () => void;
-    }
+  onClose?: () => void;
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
