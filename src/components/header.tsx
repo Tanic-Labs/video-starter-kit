f6b7723fee99df473a834a3c73a4468c4cc0104b
Ailam Cabello <ailamcabello@192.168.2.81>
2025-02-25 13:21:32 -0600

    build-v1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Button } from "@/components/ui/button";
